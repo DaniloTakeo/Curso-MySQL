ad69bc9951255031db0382970b8e1c3b80313a81
DaniloTakeo <danilot.kanizawa@gmail.com>
2023-03-17 09:19:11 -0300

    Aula 01
    Aula do conjunto de cursos sobre MySQL da Alura.

---
+++ conceitos.sql
@@ -0,0 +1,18 @@
+/* 
+VANTAGENS
+- Aprendizado
+- Portabilidade
+- Longevidade
+- Comunicação
+- Liberdade de Escolha
+
+DESVANTAGENS
+- Falta de criatividade
+- Falta de estruturação
+
+COMANDOS
+- DDL (Definição de dados)
+- DML (Manipulação de dados)
+- DCL (Controle de dados)
+
+ */ 
